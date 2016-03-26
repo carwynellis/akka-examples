04fd4c4fb0ba97f10eaf4edefad2ac89297733ff
Carwyn Ellis <carwynellis@gmail.com>
2016-03-26 18:14:01 +0000

    File IO examples with and without sink

--- src/main/scala/uk/carwynellis/akka/stream/SimpleExample.scala
+++ src/main/scala/uk/carwynellis/akka/stream/SimpleExample.scala
@@ -1,9 +1,12 @@
 package uk.carwynellis.akka.stream
 
+import java.io.File
+
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.{IOResult, ActorMaterializer}
+import akka.stream.scaladsl._
+import akka.util.ByteString
 
 import scala.concurrent.Future
 
@@ -18,10 +21,35 @@ object SimpleExample {
   def incrementingSource(n: Int): Source[Int, NotUsed] = Source(1 to n)
 
   def printNumberStream(n: Int): Unit = {
-    incrementingSource(5) runForeach(i => println(i))
+    incrementingSource(5)
+      .runForeach(i => println(i))
   }
 
   def factorial(n: Int): Future[BigInt] =
-    incrementingSource(n).runFold(BigInt(1)) { (acc: BigInt, i: Int) => acc * BigInt(i) }
+    incrementingSource(n)
+      .runFold(BigInt(1)) { (acc, i) => acc * i }
+
+  def factorialSource(n: Int): Source[BigInt, NotUsed] =
+    incrementingSource(n)
+      .scan(BigInt(1)) { (acc, i) => acc * i }
+
+  def printFactorials(n: Int) =
+    factorialSource(n)
+      .runForeach(i => println(i))
+
+  def writeFactorialsToFile(n: Int, path: String): Future[IOResult] =
+    factorialSource(n)
+      .map(num => ByteString(s"$num\n"))
+      .runWith(FileIO.toFile(new File(path)))
+
+  def lineSink(filename: String): Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(s => ByteString(s"$s\n"))
+      .toMat(FileIO.toFile(new File(filename)))(Keep.right)
+
+  def writeFactorialsToFileWithLineSink(n: Int, path: String): Future[IOResult] =
+    factorialSource(n).map(_.toString).runWith(lineSink(path))
+
+
 
 }
--- src/test/scala/uk/carwynellis/akka/stream/SimpleExampleTest.scala
+++ src/test/scala/uk/carwynellis/akka/stream/SimpleExampleTest.scala
@@ -1,10 +1,14 @@
 package uk.carwynellis.akka.stream
 
+import akka.stream.IOResult
 import org.scalatest.FunSuite
 import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.Future
 
+/**
+  * TODO - catch IO and assert (any IO is just sent to stdout/file at the moment)
+  */
 class SimpleExampleTest extends FunSuite with ScalaFutures {
 
   test("printNumberStream should output some numbers") {
@@ -19,5 +23,19 @@ class SimpleExampleTest extends FunSuite with ScalaFutures {
   test("factorial should produce expected result for 5") {
     val f: Future[BigInt] = SimpleExample.factorial(5)
     whenReady(f) { result => assert(result == BigInt(120)) }
+  }
+
+  test("print factorials should output factorials to n") {
+    SimpleExample.printFactorials(5)
+  }
+
+  test("write factorials to file returns success") {
+    val f: Future[IOResult] = SimpleExample.writeFactorialsToFile(5, "/tmp/factorials")
+    whenReady(f) { result => assert(result.wasSuccessful) }
+  }
+
+  test("write factorials to file with link sink returns success") {
+    val f: Future[IOResult] = SimpleExample.writeFactorialsToFile(5, "/tmp/factorials")
+    whenReady(f) { result => assert(result.wasSuccessful) }
   }
 }
