034dd8c66f209bc59f6fb45782b7e20db607dff3
Carwyn Ellis <carwynellis@gmail.com>
2016-05-01 10:15:57 +0100

    fix closed shape example

--- src/main/scala/uk/carwynellis/akka/stream/StreamGraphs.scala
+++ src/main/scala/uk/carwynellis/akka/stream/StreamGraphs.scala
@@ -16,21 +16,15 @@ object StreamGraphs extends App {
   val closedShapeExample = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._
     val in = Source(1 to 10)
-    // TODO - figure out how to change this to something useful without breaking the example
-    val out = Sink.ignore
+    // Note - you need to explicitly declare the type on the sink otherwise this will not compile.
+    val out = Sink.foreach[Int](i => println(i))
 
     val bcast = builder.add(Broadcast[Int](2))
     val merge = builder.add(Merge[Int](2))
 
     val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
 
-    // Simple flow to print and return values
-    val printer = Flow[Int].map { i =>
-      println(i)
-      i
-    }
-
-    in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> printer ~> out
+    in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
     bcast ~> f4 ~> merge
     ClosedShape
   })
